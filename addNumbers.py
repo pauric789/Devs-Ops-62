74fc2bcf13db9892167e45e4ba2bebac0e8fc6de
pauric789 <l00181062@atu.ie>
2025-11-05 12:10:36 +0000

    dependabot test 2

--- addNumbers.py
+++ addNumbers.py
@@ -11,8 +11,8 @@ def add_numbers(a, b):
     return a + b
 
 
-num1 = int(input("Enter first number!: "))
-num2 = int(input("Enter second number!: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 result = add_numbers(num1, num2)
 print(f"Result: {result}")
