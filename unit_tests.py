13544241d644e2271e339b10d97bfd7516c2f42d
pauric789 <l00181062@atu.ie>
2025-11-19 12:40:36 +0000

    test

---
+++ unit_tests.py
@@ -0,0 +1,31 @@
+
+import unittest
+"""Unit tests for the project."""
+
+
+
+from add_numbers import add_numbers
+
+
+class TestPositive(unittest.TestCase):
+
+    def test_add_positive_numbers(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+class TestNegative(unittest.TestCase):
+
+    def test_add_negative_numbers(self):
+        result = add_numbers(-1, -4)
+        self.assertEqual(result, -5)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+print("All tests passed!")
