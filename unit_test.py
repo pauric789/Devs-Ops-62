e8eb30c771ba7088a933b34e750bf66c3fbd0f68
pauric789 <l00181062@atu.ie>
2025-12-27 19:33:07 +0000

    update tests with fastapi

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,9 @@
 """Unit tests for the project."""
 import unittest
-from add_numbers import add_numbers
+from fastapi.testclient import TestClient
+from add_numbers import add_numbers,app
+
+client = TestClient(app)
 
 class TestPositive(unittest.TestCase):
     """Test cases for adding positive numbers."""
@@ -17,6 +20,15 @@ class TestNegative(unittest.TestCase):
         """Test adding two negative numbers returns correct sum."""
         result = add_numbers(-1, -4)
         self.assertEqual(result, -5)
+
+class TestAPI(unittest.TestCase):
+    """Test cases for the FastAPI endpoints."""
+
+    def test_api_add_endpoint(self):
+        """Test the /add web route returns JSON with the correct sum."""
+        response = client.get("/add?a=10&b=20")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"sum": 30})
 
 if __name__ == '__main__':
     unittest.main()
